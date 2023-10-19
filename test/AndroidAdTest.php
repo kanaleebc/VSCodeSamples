0e959ff494d4840c7ef0650510dab4c1900db261
Kana Lee <klee@brightcove.com  git config --global user.name kanaleebcgit config --global user.email klee@brightcove.com>
2023-10-19 11:58:30 -0700

    initial commit

---
+++ test/AndroidAdTest.php
@@ -0,0 +1,45 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+?>
+
+<VAST version="3.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="vast.xsd">
+<Ad id="SOADS_1310251" sequence="1">
+    <InLine>
+      <AdSystem>SOADS</AdSystem>
+      <AdTitle>PR</AdTitle>
+      <Description>PR ad</Description>
+      <Impression><![CDATA[https://tg.socdm.com/bc/v3?b=Y2hzbT0yNTEsZTAyNzYwOGRpZD04NTI3NyZwb3M9U1NQTE9DJmFkPTEzMTAyNTEvNTM1My4xMDA5Ny4xMzEwMjUxLzIzODk5NjUvOTM4NjkvODUyNzc6U1NQTE9DOiovaWR4PTA7cHI9eVE7cHJiPXlRO3Bybz15UTtwcm9jPUpQWTtjcmQyeT0xNDYuMTk5OTk5OTk5OTk5OTk7Y3J5MmQ9MC4wMDY4Mzk5NDUyODA0Mzc3NTczO3Bydj1panpBVm1Zb3BuYlRXUHRxaG1kQ3VlZFc0N3QyNTUwS2ZiMVZiYjdLOyZleD0xNjk2MDIyOTY0JmN0PTE2OTYwMjI5NjQ3MzEmc3I9aHR0cA--&xuid=ZRdBtMCo8X4AACpaFL0AAAAA&ctsv=m-ad424&seqid=75d40958-e37b-2134-2f10-3f2906ef2291&seqtime=1696022964731&t=.gif]]></Impression>
+      <Creatives>
+        <Creative AdID="2389965">
+          <Linear>
+            <Duration>00:00:15</Duration>
+            <TrackingEvents>
+              <Tracking event="start"><![CDATA[https://tg.socdm.com/aux/vast_event?page_id=85277&schedule_id=5353.10097.1310251&wo_schedule_id=1310251&creative_id=2389965&event=start&loglocation_id=93869&order_id=5353&lookupname=85277:SSPLOC:*&family_id=1310251&delivery_unit_id=10097&position=SSPLOC&multiad_index=0&seqid=75d40958-e37b-2134-2f10-3f2906ef2291]]></Tracking>
+              <Tracking event="firstQuartile"><![CDATA[https://tg.socdm.com/aux/vast_event?page_id=85277&schedule_id=5353.10097.1310251&wo_schedule_id=1310251&creative_id=2389965&event=firstQuartile&loglocation_id=93869&order_id=5353&lookupname=85277:SSPLOC:*&family_id=1310251&delivery_unit_id=10097&position=SSPLOC&multiad_index=0&seqid=75d40958-e37b-2134-2f10-3f2906ef2291]]></Tracking>
+              <Tracking event="midpoint"><![CDATA[https://tg.socdm.com/aux/vast_event?page_id=85277&schedule_id=5353.10097.1310251&wo_schedule_id=1310251&creative_id=2389965&event=midpoint&loglocation_id=93869&order_id=5353&lookupname=85277:SSPLOC:*&family_id=1310251&delivery_unit_id=10097&position=SSPLOC&multiad_index=0&seqid=75d40958-e37b-2134-2f10-3f2906ef2291]]></Tracking>
+              <Tracking event="thirdQuartile"><![CDATA[https://tg.socdm.com/aux/vast_event?page_id=85277&schedule_id=5353.10097.1310251&wo_schedule_id=1310251&creative_id=2389965&event=thirdQuartile&loglocation_id=93869&order_id=5353&lookupname=85277:SSPLOC:*&family_id=1310251&delivery_unit_id=10097&position=SSPLOC&multiad_index=0&seqid=75d40958-e37b-2134-2f10-3f2906ef2291]]></Tracking>
+              <Tracking event="complete"><![CDATA[https://tg.socdm.com/aux/vast_event?page_id=85277&schedule_id=5353.10097.1310251&wo_schedule_id=1310251&creative_id=2389965&event=complete&loglocation_id=93869&order_id=5353&lookupname=85277:SSPLOC:*&family_id=1310251&delivery_unit_id=10097&position=SSPLOC&multiad_index=0&seqid=75d40958-e37b-2134-2f10-3f2906ef2291]]></Tracking>
+              <Tracking event="complete"><![CDATA[https://tr.ultraimpression.jp/ads/vast_event/pixel.png?streaming_type=vod&r=91b9f6a4-570e-757c-fb07-03fb0232f928&device=pc&dcid=&ppid=j5apatkbtpw633824578&is_lat=&site=tver&pf=1028&page_id=85277&vid=6336792830112&advertiser_id=3159&order_id=5353&schedule_id=5353.10097.1310251&creative_id=2389965&position=85277:SSPLOC:*&v_pos=preroll&sequence_id=75d40958-e37b-2134-2f10-3f2906ef2291&label_tvcu_age=23&label_tvcu_agegrp=3&label_tvcu_aud=&label_tvcu_ccode=13214&label_tvcu_g=m&label_tvcu_pcode=13&label_tvcu_zcode=1850023&label_tvcu_interest=&splice_insert_message=&personal_is_lat=0&platform_uid=ffa032c1-8fce-445b-aeb7-b17fa08691b9&member_id=&uid=ZRdBtMCo8X4AACpaFL0AAAAA&metrics=100]]></Tracking>
+              <Tracking event="complete"><![CDATA[https://v-img-ad.interactive-circle.jp/live/trackadc/?a=10216716&c=vod&r=bf8e6d0d-5af2-e586-c4ad-a98c45026dc6&t=browser&appid=dio_product&dcid=&dnt=&dcos=pc&pf=1028&site=tver&pc=6336792830112&ad=2389965&roll=preroll&luid=&u1=j5apatkbtpw633824578&u2=ad&metrics=100]]></Tracking>
+              <Tracking event="firstQuartile"><![CDATA[https://v-img-ad.interactive-circle.jp/live/trackadc/?a=10216716&c=vod&r=3ab09569-7ad3-0099-ea6a-2112cfbf5fa2&t=browser&appid=dio_product&dcid=&dnt=&dcos=pc&pf=1028&site=tver&pc=6336792830112&ad=2389965&roll=preroll&luid=&u1=j5apatkbtpw633824578&u2=ad&metrics=25]]></Tracking>
+              <Tracking event="midpoint"><![CDATA[https://v-img-ad.interactive-circle.jp/live/trackadc/?a=10216716&c=vod&r=05b0ccbf-342d-1809-b178-257647c46d55&t=browser&appid=dio_product&dcid=&dnt=&dcos=pc&pf=1028&site=tver&pc=6336792830112&ad=2389965&roll=preroll&luid=&u1=j5apatkbtpw633824578&u2=ad&metrics=50]]></Tracking>
+              <Tracking event="start"><![CDATA[https://tr.ultraimpression.jp/ads/vast_event/pixel.png?streaming_type=vod&r=113bac4f-6d68-6894-1763-14f7ec7b8895&device=pc&dcid=&ppid=j5apatkbtpw633824578&is_lat=&site=tver&pf=1028&page_id=85277&vid=6336792830112&advertiser_id=3159&order_id=5353&schedule_id=5353.10097.1310251&creative_id=2389965&position=85277:SSPLOC:*&v_pos=preroll&sequence_id=75d40958-e37b-2134-2f10-3f2906ef2291&label_tvcu_age=23&label_tvcu_agegrp=3&label_tvcu_aud=&label_tvcu_ccode=13214&label_tvcu_g=m&label_tvcu_pcode=13&label_tvcu_zcode=1850023&label_tvcu_interest=&splice_insert_message=&personal_is_lat=0&platform_uid=ffa032c1-8fce-445b-aeb7-b17fa08691b9&member_id=&uid=ZRdBtMCo8X4AACpaFL0AAAAA&metrics=0]]></Tracking>
+              <Tracking event="start"><![CDATA[https://v-img-ad.interactive-circle.jp/live/trackadc/?a=10216716&c=vod&r=6cb14e06-a0f6-0dd2-6c7c-c2c297712f03&t=browser&appid=dio_product&dcid=&dnt=&dcos=pc&pf=1028&site=tver&pc=6336792830112&ad=2389965&roll=preroll&luid=&u1=j5apatkbtpw633824578&u2=ad&metrics=0]]></Tracking>
+              <Tracking event="thirdQuartile"><![CDATA[https://v-img-ad.interactive-circle.jp/live/trackadc/?a=10216716&c=vod&r=6ee6ab8a-6c56-d759-a7ae-886a7fb315eb&t=browser&appid=dio_product&dcid=&dnt=&dcos=pc&pf=1028&site=tver&pc=6336792830112&ad=2389965&roll=preroll&luid=&u1=j5apatkbtpw633824578&u2=ad&metrics=75]]></Tracking>
+            </TrackingEvents>
+            <VideoClicks>
+              <ClickThrough/>
+            </VideoClicks>
+            <MediaFiles>
+              <MediaFile bitrate="600" delivery="progressive" height="360" type="video/mp4" width="640">https://i.socdm.com/a/2/2333/2442789/20230912053029-ef198cf3b26222.mp4</MediaFile>
+              <MediaFile bitrate="900" delivery="progressive" height="480" type="video/mp4" width="854">https://i.socdm.com/a/2/2333/2442790/20230912053030-0acaffe595ffec.mp4</MediaFile>
+              <MediaFile bitrate="2000" delivery="progressive" height="720" type="video/mp4" width="1280">https://i.socdm.com/a/2/2333/2442791/20230912053030-2461c6fe85629b.mp4</MediaFile>
+              <MediaFile bitrate="3500" delivery="progressive" height="1080" type="video/mp4" width="1920">https://i.socdm.com/a/2/2333/2442792/20230912053031-f86ab377e8f220.mp4</MediaFile>
+            </MediaFiles>
+          </Linear>
+        </Creative>
+      </Creatives>
+    </InLine>
+  </Ad></VAST>
+  
